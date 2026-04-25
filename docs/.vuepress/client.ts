74fdaf9311a7c2b1b2e9f9e48ee46206c061476f
seepine <seepine@outlook.com>
2026-04-25 16:34:47 +0800

    feat(docs): add theme mode attribute sync for dark mode

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,5 +1,6 @@
 import { defineClientConfig } from '@vuepress/client'
 import NaiveUi from 'naive-ui'
+import { onMounted } from 'vue'
 import Layout from './layouts/Layout.vue'
 import './configs/styles/index.scss'
 
@@ -15,5 +16,28 @@ export default defineClientConfig({
     app.use(NaiveUi)
     app.use(Ui)
   },
-  setup() {},
+  setup() {
+    onMounted(() => {
+      const htmlEl = document.documentElement
+      const changeTheme = () => {
+        const isDark = htmlEl.classList.contains('dark')
+        if (isDark) {
+          htmlEl.setAttribute('theme-mode', 'dark')
+        } else {
+          htmlEl.removeAttribute('theme-mode')
+        }
+      }
+
+      const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+          if (mutation.attributeName === 'class') {
+            changeTheme()
+          }
+        })
+      })
+      observer.observe(htmlEl, { attributes: true })
+      // 手动触发一次
+      changeTheme()
+    })
+  },
 })
